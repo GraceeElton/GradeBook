c6ff501bc4ceeef17804ed5cbd628890e0ee891c
GraceeElton <60789216+GraceeElton@users.noreply.github.com>
2020-12-08 14:07:28 -0700

    Added unit test to my gradebook

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using System;
+
+
+namespace GradeBook
+{
+    public class Book
+    {
+
+
+
+        // constructor 
+        public Book(string name)
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        // methods 
+        public void AddGrade(double grade)
+        {
+            grades.Add(grade);
+        }
+
+
+        // use the class name as the name so it is an instance of that class.
+        //object of tpye of Statistics.
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            foreach (var grade in grades)
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
+
+
+            }
+            result.Average /= grades.Count;
+            return result;
+        }
+
+
+        // feilds 
+        private List<double> grades;
+        private string name;
+
+    }
+}
---
+++ test/GradeBook.tests/BookTests.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace GradeBook.tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arange test data 
+            var book = new Book("");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.3);
+
+
+            //act invoke code for result 
+            var result = book.GetStatistics();
+
+            //assert idk hah 
+            Assert.Equal(85.6, result.Average, 1);
+            Assert.Equal(90.5, result.High, 1);
+            Assert.Equal(77.3, result.Low, 1);
+
+        }
+    }
+}
